9f8b8f0d27b7cfa2b66276d800e04d58c4520d72
fufukha <wafakhalid29@gmail.com>
2019-07-24 19:17:49 -0400

    purified board reducer by adding random indices to board actions

--- src/actions/boardActions.js
+++ src/actions/boardActions.js
@@ -7,6 +7,32 @@ const defaultConfig = {
 export const configureBoard = (config=defaultConfig) => {
     return {
         type: 'CONFIGURE_BOARD',
-        payload: config
+        payload: {
+            rows: config.rows,
+            columns: config.columns,
+            mines: randomIndices(config.rows, config.columns, config.numOfMines)
+        }
     }
+}
+
+function randomIndices(rows, columns, numberOfIndices) {
+    let count = numberOfIndices + 1;
+    const randomIndices = {};
+
+    while(--count) {
+        const i = randomInteger(rows);
+        const j = randomInteger(columns);
+
+        if(randomIndices[i] && randomIndices[i][j]) {
+			count++;
+		} else {
+			if(randomIndices[i] === undefined) randomIndices[i] = {};
+			randomIndices[i][j] = true;
+		}
+    }
+    return randomIndices;
+}
+
+function randomInteger(max) {
+    return Math.floor(Math.random() * max);
 }
--- src/reducers/boardReducer.js
+++ src/reducers/boardReducer.js
@@ -15,33 +15,11 @@ const boardReducer = (state=initialState, action) => {
                     ...state.board,
                     rows: action.payload.rows,
                     columns: action.payload.columns,
-                    mines: randomIndices(action.payload.rows, action.payload.columns, action.payload.numOfMines)
+                    mines: action.payload.mines
                 }
             }
     }
     return state;
-}
-
-function randomIndices(rows, columns, numberOfIndices) {
-    let count = numberOfIndices + 1;
-    const randomIndices = {};
-
-    while(--count) {
-        const i = randomInteger(rows);
-        const j = randomInteger(columns);
-
-        if(randomIndices[i] && randomIndices[i][j]) {
-			count++;
-		} else {
-			if(randomIndices[i] === undefined) randomIndices[i] = {};
-			randomIndices[i][j] = true;
-		}
-    }
-    return randomIndices;
-}
-
-function randomInteger(max) {
-    return Math.floor(Math.random() * max);
 }
 
 export default boardReducer;
