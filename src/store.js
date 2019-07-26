babdf8189cfc2b1d7ccdfd384033add46fade9e1
fufukha <wafakhalid29@gmail.com>
2019-07-25 21:38:04 -0400

    incorporated rootReducer in store

--- src/store.js
+++ src/store.js
@@ -1,11 +1,10 @@
 import { compose, applyMiddleware, createStore } from 'redux';
 import logger from 'redux-logger';
 import thunk from 'redux-thunk';
-//import rootReducer from './reducers';
-import boardReducer from './reducers/boardReducer'
+import rootReducer from './reducers';
 
 export default createStore(
-    boardReducer,
+    rootReducer,
     compose(
         applyMiddleware(logger, thunk),
         window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
