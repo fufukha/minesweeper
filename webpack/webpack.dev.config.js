ab5826e655ae7b762f6f7a829854645a9a464747
fufukha <wafakhalid29@gmail.com>
2019-08-05 13:58:29 -0400

    remove sass loaders and added css modules

--- webpack/webpack.dev.config.js
+++ webpack/webpack.dev.config.js
@@ -3,65 +3,65 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: {
-        minesweeper: './src/minesweeper.js'
-    },
-    output: {
-        filename: '[name].js',
-        path: path.resolve(__dirname, '../dist'),
-        publicPath: ''
-    },
-    mode: 'development',
-    devServer: {
-        contentBase: path.resolve(__dirname, '../dist'),
-        index: 'index.html',
-        port: 9000
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(png|svg|jpe?g|gif)$/,
-                use: [
-                    'file-loader'
-                ]
-            },
-            {
-                test: /\.css$/,
-                use: [
-                    'style-loader', 'css-loader'
-                ]
-            },
-            {
-                test: /\.scss$/,
-                use: [
-                    'style-loader', 'css-loader', 'sass-loader'
-                ]
-            },
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: [
-                            '@babel/preset-env', '@babel/preset-react'
-                        ]
-                    }
-                }
-            }
+  entry: {
+    minesweeper: './src/minesweeper.js'
+  },
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, '../dist'),
+    publicPath: ''
+  },
+  mode: 'development',
+  devServer: {
+    contentBase: path.resolve(__dirname, '../dist'),
+    index: 'index.html',
+    port: 9000
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(png|svg|jpe?g|gif)$/,
+        use: [
+          'file-loader'
         ]
-    },
-    plugins: [
-        new CleanWebpackPlugin(),
-        new HtmlWebpackPlugin({
-            filename: 'minesweeper.html',
-            chunks: ['minesweeper'],
-            title: 'Hello world',
-            meta: {
-                viewpoint: 'width=device-width, initial-scale=1',
-                charset: 'utf-8'
-            },
-            template: 'src/template.html'
-        })
+      },
+      {
+        test: /\.css$/,
+        use: [
+					'style-loader',
+					{
+						loader: 'css-loader',
+						options: {
+							modules: true,
+						},
+					}
+				]
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+            loader: 'babel-loader',
+            options: {
+                presets: [
+                  '@babel/preset-env', '@babel/preset-react'
+                ]
+            }
+        }
+      }
     ]
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      filename: 'minesweeper.html',
+      chunks: ['minesweeper'],
+      title: 'Hello world',
+      meta: {
+          viewpoint: 'width=device-width, initial-scale=1',
+          charset: 'utf-8'
+      },
+      template: 'src/template.html'
+    })
+  ]
 }
--- webpack/webpack.production.config.js
+++ webpack/webpack.production.config.js
@@ -4,70 +4,70 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: {
-        minesweeper: './src/minesweeper.js'
-    },
-    output: {
-        filename: '[name].[contenthash].js',
-        path: path.resolve(__dirname, '../dist'),
-        publicPath: '/static/'
-    },
-    mode: 'production',
-    optimization: {
-        splitChunks: {
-            chunks: 'all',
-            minSize: 5000,
-            automaticNameDelimiter: '_'
-        }
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(png|svg|jpe?g|gif)$/,
-                use: [
-                    'file-loader'
-                ]
-            },
-            {
-                test: /\.css$/,
-                use: [
-                    MiniCssExtractPlugin.loader, 'css-loader'
-                ]
-            },
-            {
-                test: /\.scss$/,
-                use: [
-                    MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'
-                ]
-            },
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: [
-                            '@babel/preset-env', '@babel/preset-react'
-                        ]
-                    }
-                }
-            }
+  entry: {
+    minesweeper: './src/minesweeper.js'
+  },
+  output: {
+    filename: '[name].[contenthash].js',
+    path: path.resolve(__dirname, '../dist'),
+    publicPath: '/static/'
+  },
+  mode: 'production',
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+      minSize: 5000,
+      automaticNameDelimiter: '_'
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(png|svg|jpe?g|gif)$/,
+        use: [
+          'file-loader'
         ]
-    },
-    plugins: [
-        new MiniCssExtractPlugin({
-            filename: '[name].[contenthash].css'
-        }),
-        new CleanWebpackPlugin(),
-        new HtmlWebpackPlugin({
-            filename: 'minesweeper.html',
-            chunks: ['minesweeper'],
-            title: 'Hello world',
-            meta: {
-                viewpoint: 'width=device-width, initial-scale=1',
-                charset: 'utf-8'
-            },
-            template: 'src/template.html'
-        })
+      },
+      {
+        test: /\.css$/,
+        use: [
+					MiniCssExtractPlugin.loader,
+					{
+						loader: 'css-loader',
+						options: {
+							modules: true,
+						},
+					}
+				]
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+              presets: [
+                '@babel/preset-env', '@babel/preset-react'
+              ]
+          }
+        }
+      }
     ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '[name].[contenthash].css'
+    }),
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      filename: 'minesweeper.html',
+      chunks: ['minesweeper'],
+      title: 'Hello world',
+      meta: {
+        viewpoint: 'width=device-width, initial-scale=1',
+        charset: 'utf-8'
+      },
+      template: 'src/template.html'
+    })
+  ]
 };
